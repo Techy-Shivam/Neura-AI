e58063e562d9e8998c42763289ef77d651839b44
Techy-Shivam <your_github_email@example.com>
2025-08-29 15:48:04 +0530

    Add publi

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
         setSubmitting(true);
         console.log(form);
 
-        axios.post("http://localhost:3000/api/auth/register", {
+        axios.post("https://neura-ai-3.onrender.com//api/auth/register", {
             email: form.email,
             fullName: {
                 firstName: form.firstname,
